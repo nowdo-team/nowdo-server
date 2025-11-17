4703ab34b15a9fa26cfde0b981b3642261cc7580
WATCHHONG <gihong0101@gmail.com>
2025-11-17 20:02:39 +0900

    User domain set

--- src/main/java/com/hsjh/nowdo/repository/UserRepository.java
+++ src/main/java/com/hsjh/nowdo/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.hsjh.nowdo.repository;
 
-public interface UserRepository {
+import com.hsjh.nowdo.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
