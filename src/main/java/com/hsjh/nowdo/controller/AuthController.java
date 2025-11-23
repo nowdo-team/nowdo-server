2ac6bc3211208281532d0b6c50f3faadde288165
PR0623-pixel <fksfksfn623@gmail.com>
2025-11-23 17:05:05 +0900

    11/23

--- src/main/java/com/hsjh/nowdo/controller/AuthController.java
+++ src/main/java/com/hsjh/nowdo/controller/AuthController.java
@@ -2,6 +2,42 @@ package com.hsjh.nowdo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hsjh.nowdo.dto.auth.LoginRequest;
+import com.hsjh.nowdo.dto.user.UserResponse;
+import com.hsjh.nowdo.service.AuthService;
+import com.hsjh.nowdo.service.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 @RestController
 public class AuthController {
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    public AuthController(AuthService authService, UserService userService){
+        this.authService = authService;
+        this.userService = userService;
+    }
+
+    //로그인 UserId 반환
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(@RequestBody LoginRequest request) {
+        UserResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+    
+    //마이페이지 조회 (경로 /me/추가)
+    @GetMapping("/me/{userId}")
+    public ResponseEntity<UserResponse> getMyInfo(@RequestParam Long userId) {
+        UserResponse response = userService.getMyInfo(userId);
+        return ResponseEntity.ok(response);
+    }
+    
 }
