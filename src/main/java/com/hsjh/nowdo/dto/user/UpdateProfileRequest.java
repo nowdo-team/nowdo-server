6df9f98d3bb3339bce6643f3999dc3e29a4a4b1b
PR0623-pixel <fksfksfn623@gmail.com>
2025-11-23 17:05:05 +0900

    11/23

--- src/main/java/com/hsjh/nowdo/dto/user/UpdateProfileRequest.java
+++ src/main/java/com/hsjh/nowdo/dto/user/UpdateProfileRequest.java
@@ -1,4 +1,15 @@
 package com.hsjh.nowdo.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class UpdateProfileRequest {
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+
+    // 프로필 이미지는 선택값이어서 validation 미구현
+    private String profileImg;
+
+    public String getNickname(){ return nickname; }
+    public String getProfileImg() { return profileImg; }
 }
---
+++ src/main/java/com/hsjh/nowdo/service/AuthService.java
@@ -0,0 +1,33 @@
+package com.hsjh.nowdo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.hsjh.nowdo.domain.user.User;
+import com.hsjh.nowdo.dto.auth.LoginRequest;
+import com.hsjh.nowdo.dto.user.UserResponse;
+import com.hsjh.nowdo.repository.UserRepository;
+
+
+// 11/23일 추가 작업, AuthService의 추가 작성 필요에 따라 작성.
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    //로그인 기능 (클라이언트)
+    public UserResponse login(LoginRequest request){
+        User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."));
+
+        if(!user.getPassword().equals(request.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        return UserResponse.from(user);
+    }
+
+}
