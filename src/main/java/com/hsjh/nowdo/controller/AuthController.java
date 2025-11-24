5cfca04285dc305597e0fcd0000110f27bd7a822
PR0623-pixel <fksfksfn623@gmail.com>
2025-11-24 20:03:39 +0900

    Beta_Domain_settings

--- src/main/java/com/hsjh/nowdo/controller/AuthController.java
+++ src/main/java/com/hsjh/nowdo/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.hsjh.nowdo.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hsjh.nowdo.dto.auth.LoginRequest;
+import com.hsjh.nowdo.dto.user.UserRegisterRequest;
 import com.hsjh.nowdo.dto.user.UserResponse;
 import com.hsjh.nowdo.service.AuthService;
 import com.hsjh.nowdo.service.UserService;
@@ -10,12 +11,14 @@ import com.hsjh.nowdo.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
@@ -26,6 +29,12 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRegisterRequest request){
+        UserResponse response = userService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
     //로그인 UserId 반환
     @PostMapping("/login")
     public ResponseEntity<UserResponse> login(@RequestBody LoginRequest request) {
@@ -33,8 +42,8 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
     
-    //마이페이지 조회 (경로 /me/추가)
-    @GetMapping("/me/{userId}")
+    //마이페이지 조회 (경로 /auth/추가 11/24)
+    @GetMapping("/{userId}")
     public ResponseEntity<UserResponse> getMyInfo(@RequestParam Long userId) {
         UserResponse response = userService.getMyInfo(userId);
         return ResponseEntity.ok(response);
