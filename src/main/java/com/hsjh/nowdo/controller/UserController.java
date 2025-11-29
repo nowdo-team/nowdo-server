cbf145f5635afad9892cbe7ed909d2a11c307adc
PR0623-pixel <fksfksfn623@gmail.com>
2025-11-29 09:59:34 +0900

    웹api 연동

--- src/main/java/com/hsjh/nowdo/controller/UserController.java
+++ src/main/java/com/hsjh/nowdo/controller/UserController.java
@@ -2,16 +2,20 @@ package com.hsjh.nowdo.controller;
 
 import com.hsjh.nowdo.dto.user.UserRegisterRequest;
 import com.hsjh.nowdo.dto.user.UserResponse;
+import com.hsjh.nowdo.common.exception.UnauthorizedException;
 import com.hsjh.nowdo.dto.user.UpdateProfileRequest;
 import com.hsjh.nowdo.service.UserService;
+import com.hsjh.nowdo.dto.auth.LoginRequest;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -32,22 +36,62 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    //로그인
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(
+        @RequestBody LoginRequest request, HttpServletRequest httpServletRequest) 
+    {
+    UserResponse response =
+            userService.login(request.getEmail(), request.getPassword());
+
+    HttpSession session = httpServletRequest.getSession(true);
+    session.setAttribute("userId", response.getId());
+
+    return ResponseEntity.ok(response);
+    }
+
+
+    //로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null){
+            session.invalidate();
+        }
+        return ResponseEntity.ok("로그아웃 완료");
+    }
+    
+
 
     //마이페이지 조회 기능 (11/20 추가)
-    @PostMapping("/me")
-    public ResponseEntity<UserResponse> getMyInfo(
-        @AuthenticationPrincipal Long userId){
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getMyInfo(HttpServletRequest request){
 
+            HttpSession session = request.getSession(false);
+
+            if(session == null || session.getAttribute("userId") == null){
+                throw new UnauthorizedException("로그인이 필요합니다.");
+            }
+
+            Long userId = (Long) session.getAttribute("userId");
             UserResponse response = userService.getMyInfo(userId);
+
             return ResponseEntity.ok(response);
         }
     
     //프로필 수정 (11/20 추가)
     @PutMapping("/me")
-    public ResponseEntity<UserResponse> updateProfile (@AuthenticationPrincipal Long userId,
-         @Valid @RequestBody UpdateProfileRequest request) {
+    public ResponseEntity<UserResponse> updateProfile (HttpServletRequest request,
+         @Valid @RequestBody UpdateProfileRequest updateRequest) {
+            HttpSession session = request.getSession(false);
+
+            if(session == null || session.getAttribute("userId")== null){
+                throw new UnauthorizedException("로그인이 필요합니다.");
+            }
+
+            Long userId = (Long) session.getAttribute("userId");
         
-            UserResponse response = userService.updateProfile(userId, request);
+            UserResponse response = userService.updateProfile(userId, updateRequest);
             return ResponseEntity.ok(response);
     
     }
--- src/main/java/com/hsjh/nowdo/service/UserService.java
+++ src/main/java/com/hsjh/nowdo/service/UserService.java
@@ -1,11 +1,14 @@
 package com.hsjh.nowdo.service;
 
+import com.hsjh.nowdo.common.exception.NotFoundException;
+import com.hsjh.nowdo.common.exception.UnauthorizedException;
 import com.hsjh.nowdo.domain.user.User;
 import com.hsjh.nowdo.domain.user.UserStatus;
 import com.hsjh.nowdo.dto.user.UpdateProfileRequest;
 import com.hsjh.nowdo.dto.user.UserRegisterRequest;
 import com.hsjh.nowdo.dto.user.UserResponse;
 import com.hsjh.nowdo.repository.UserRepository;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,20 +44,44 @@ public class UserService {
         // DTO로 변환해서 반환
         return UserResponse.from(saved);
     }
+
+    // 로그인 기능
+    public UserResponse login(String email, String password) {
+    User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new NotFoundException("이메일이 존재하지 않습니다."));
+
+    if (!user.getPassword().equals(password)) {
+        throw new UnauthorizedException("비밀번호가 일치하지 않습니다.");
+    }
+
+    return UserResponse.from(user);
+    }
+
+
     // 마이페이지 조회
     public UserResponse getMyInfo (Long userId){
+
+        if(userId == null){
+            throw new UnauthorizedException("로그인이 필요합니다.");
+        }
+
         User user = userRepository.findById(userId).orElseThrow(()->
-        new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        new NotFoundException("존재하지 않는 사용자입니다."));
 
         return UserResponse.from(user);
     }
     // 프로필 수정
-    public UserResponse updateProfile(Long userId, UpdateProfileRequest request){
+    public UserResponse updateProfile(Long userId, UpdateProfileRequest updateRequest){
+
+        if(userId == null){
+            throw new UnauthorizedException("로그인이 필요합니다.");
+        }
+
         User user = userRepository.findById(userId).orElseThrow(()-> 
         new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-        user.setNickname(request.getNickname());
-        user.setProfileImg(request.getProfileImg());
+        user.setNickname(updateRequest.getNickname());
+        user.setProfileImg(updateRequest.getProfileImg());
 
         User updated = userRepository.save(user);
 
